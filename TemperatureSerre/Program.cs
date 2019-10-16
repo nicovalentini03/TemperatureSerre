89975442d747dca86de02209e62cfbd34b9ea804
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:41:30 +0200

    cambiamento

--- TemperatureSerre/Program.cs
+++ TemperatureSerre/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TemperatureSerre
 {
     class Program
     {
         static void Main(string[] args)
+      
         {
+            Console.WriteLine("inserisci la temperatura della serra");
+            double t = double.Parse(Console.ReadLine());
+            if (t == 18)
+            {
+                Console.WriteLine("la temperatura della serra è normale");
+            }
+            if (t < 5)
+            {
+                Console.WriteLine("la temperatura della serra provoca danni irreparabili");
+            }
+            if (t < 18 && t > 5)
+            {
+                Console.WriteLine("la serra è in una situazione pericolosa");
+            }
+            Console.ReadLine();
+            
         }
     }
 }
